3807aa04e989a5ae361f8b4658c272577bd80e6e
ChrichriR <chriss_s@hotmail.fr>
2022-04-15 10:02:23 +0200

    create another way to get some simpsons quotes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplaySimpson from './components/DisplaySimpson';
+import axios from 'axios';
+import React from 'react';
+
+const sampleSimpson = {
+  character : "Homer Simpson",
+  image: 	"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+  quote: 	"Gah, stupid sexy Flanders!",
+};
 
 function App() {
+  const [simpson, setSimpson] = React.useState(sampleSimpson);
+  const getSimpson = () => {
+    axios
+    .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+    .then((response) => response.data)
+    .then((data) => {
+      setSimpson(data[0]);
+    });
+  
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DisplaySimpson simpson={simpson} />
+      <button type="button" onClick={getSimpson}>Get another one !</button>
     </div>
   );
 }
